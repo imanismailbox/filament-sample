d1423d411e0d529e2c72beecf2106486b2a95399
iman ismail <imanismailbox@gmail.com>
2024-08-20 16:03:01 +0700

    add code

---
+++ bootstrap/providers.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\AdminPartnerPanelProvider::class,
+    App\Providers\Filament\PartnerPanelProvider::class,
+];
